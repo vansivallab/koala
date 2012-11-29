a66e9c6b22f4848510874b186d02b63ede2b6195
Vansi Vallabhaneni <vansivallab@gmail.com>
2012-11-29 15:44:40 -0500

    convert UI to jquery

--- www/js/paint/ui.js
+++ www/js/paint/ui.js
@@ -1,45 +1,42 @@
 var paint = new Paint("imageDelta", "imageTmp");
+paint.toolbox.setColor('black');
 paint.toolbox.setWidth(5);
 paint.toolbox.setOpacity(1);
 
 paint.toolbox.setMode("pencil");
-var selectedBrush = "pencil";
+
+
+var $show = $("#show");
+var $hide = $("#hide");
+var $toolbar = $("#toolbar");
+var $oldBrush = $('#pencil');
 
 /*resets needed */
-document.getElementById("showHide").style.background = "#7080D7";
-document.getElementById("show").style.display = "block";
+$("#showHide").css('background', "#7080D7");
+$show.css('display', "block");
+
+
+$show.on('click touchstart', function() {
+	$show.css('display', "none");
+    $hide.css('display', "block");
+    $toolbar.css('display', "block");
+	
+});
+
+$hide.on('click touchstart', function() {
+	$show.css('display', "block");
+	$hide.css('display', "none");
+	$toolbar.css('display', "none");
+	
+});
 
 /* select tool */
-function select(node) {
-    //remove selected
-    var old = document.getElementById(selectedBrush);
-    old.className="brush";
-    node.className = "selected brush";
-    selectedBrush = node.id;
-    paint.toolbox.setMode(selectedBrush);
-    
-    
-}
-
-
-/*showHide */
-function showHide(node) {
-    var show = document.getElementById("show");
-    var hide = document.getElementById("hide");
-    var toolbar = document.getElementById("toolbar");
-    
-    if(node.id == 'show') {
-        show.style.display = 'none';
-        hide.style.display = 'block';
-        toolbar.style.display = 'block';
-        
-    } else if(node.id == 'hide') {
-        show.style.display = 'block';
-        hide.style.display = 'none';
-        toolbar.style.display = 'none';
-    
-    }
-}    
+$('.brush').on('click touchstart', function() {
+	$oldBrush.removeClass('selected');
+	$(this).addClass('selected');
+	paint.toolbox.setMode($(this).attr('id'));
+	$oldBrush = $(this);
+});
     
     
 
