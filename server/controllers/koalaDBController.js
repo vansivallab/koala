427eab7f88fa408fafb4d18ab2db7f536820e395
Alex Guo <alexchesskid@msn.com>
2012-12-08 12:38:22 -0500

    something

---
+++ server/controllers/koalaDBController.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var UserController = require('./userController.js');
+var Util = require('../util.js');
+
+function KoalaDB(connection) {
+	mongoose.connect(connection);
+	this.connections = [];
+}
+
+KoalaDB.prototype.addUser = function(username, password/*, superuser*/, callback) {
+	UserController.newUser(username, password/*, superuser*/, callback);
+	//need to validate data
+};
+
+KoalaDB.prototype.getUser = function(searchJSON, callback) {
+	User.find(searchJSON, function(err, results) {
+		if(err) {throw err;}
+		if(Util.exists(callback)) {return callback(result);}
+	});
+};
+
+KoalaDB.prototype.clearData = function(callback) {
+	User.find({}).remove();
+	if(Util.exists(callback)) {callback();}
+};
+
+KoalaDB.prototype.getCanvases = function(searchJSON, userId, callback) {
+	if(!searchJSON.userIds) {searchJSON.userIds = [userId];}
+	else if(searchJSON.userIds.find(userId) === -1) 
+	{searchJSON.userIds.push(userId);}
+	
+	return Canvas.find(searchJSON, function(err, results) {
+		if(err) {throw err;}
+		if(Util.exists(callback)) {return callback(results);}
+	});
+};
+
+//or register
+KoalaDB.prototype.login = function(username, password, callback) {
+	console.log("KBController line 39 loggin in");
+	UserController.authUser(username, password, function(validity, userObj) {
+		var retData = {validConn: validity !== -1};
+		
+		//if user doesnt exist, make one
+		if(validity === 0) {
+			return this.addUser(username, password, function(newUserObj) {
+				return callback(newUserObj, retData); //assume user creation sucessful
+			});
+		}
+		else if(validity){
+			userObj.lastLoginTimestamp = new Date();
+			return callback(userObj, retData);
+		}
+		else {
+			return callback({}, retData);
+		}
+	}.bind(this));
+};
+
+module.exports = KoalaDB;
---
+++ server/controllers/userController.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+var User = require('../models/userModel.js');
+var Util = require('../util.js');
+
+
+var UserController = {
+	newUser: function(username, password, /*superuser,*/ callback) {
+		User.findOne({username: username}, function(err, user) {
+			if(err) {throw err;}
+			
+			if(!Util.exists(user)) {
+				user = new User({
+					username: username,
+					/*superuser: superuser,*/
+					password: password, 
+					userCanvasIds: []
+				});
+				
+				user.lastLoginTimestamp = result.registeredTimestamp = new Date();
+				user.save(function(err) {
+					if(err) {throw err;}
+					return callback(user);
+				});
+				
+			}
+			else {return callback(user);}
+		});
+	},
+
+	//validity is 1 if authorized
+	//validity is 0 if username not found
+	//validity is -1 if username found, but password doesnt match
+	authUser: function(username, password, callback) {
+		User.findOne({username: username}, function(err, result) {
+			if(err) {
+			console.log(err);
+			throw err;}
+			console.log("line36 urname: "+username+" result: ");
+			console.log(JSON.stringify(result));
+			console.log("-------------\n");
+			
+			var validity = 0;
+			if(Util.exists(result)) {
+				if(Util.exists(result.password) && result.password === password) 
+				{validity = 1;}
+				
+				else {validity = -1;}
+			}
+			
+			if(Util.exists(callback)) {
+				return callback(validity, result);
+			}
+		});
+	}
+}
+
+module.exports = UserController;
